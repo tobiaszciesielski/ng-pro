0ca60c3669a3c4d15444e2c8468ee0f22d1328f0
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-11-28 16:32:38 +0100

    Creating embededView from class level

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,7 +4,8 @@ import {
   ChangeDetectorRef,
   Component,
   ComponentFactoryResolver,
-  OnInit,
+  ComponentRef,
+  TemplateRef,
   ViewChild,
   ViewContainerRef,
 } from '@angular/core';
@@ -18,7 +19,13 @@ import { SimpleFormComponent } from './components/simple-form/simple-form.compon
       <h2 class="dashboard__title">Dashboard Component</h2>
       <dashboard-auth-panel class="dashboard__forms"></dashboard-auth-panel>
       <h2 class="dashboard__title">Simple form component</h2>
-      <div #simpleForm></div>
+      <button (click)="handleDeleteComponent()">Destroy component</button>
+      <button (click)="handleMoveComponent()">Move component</button>
+      <ng-template #simpleForm></ng-template>
+
+      <ng-template #template let-n="name" let-sdwadsdw
+        >Name: {{ n }} Something: {{ sdwadsdw }}</ng-template
+      >
     </div>
   `,
 })
@@ -26,22 +33,43 @@ export class DashboardComponent implements AfterViewInit {
   @ViewChild('simpleForm', { read: ViewContainerRef })
   simpleForm!: ViewContainerRef;
 
+  @ViewChild('template') templateView!: TemplateRef<any>;
+
+  component!: ComponentRef<SimpleFormComponent>;
+
   constructor(
     private resolver: ComponentFactoryResolver,
     private cd: ChangeDetectorRef
   ) {}
 
   ngAfterViewInit(): void {
+    this.simpleForm.createEmbeddedView(this.templateView, {
+      name: 'Tobiasz',
+      $implicit: 'Ciesielski',
+    });
+
     const authFormFactory =
       this.resolver.resolveComponentFactory(SimpleFormComponent);
 
     if (this.simpleForm) {
-      const component = this.simpleForm.createComponent(authFormFactory);
-      component.instance.title = 'Changed title from dashboard';
-      component.instance.onSubmit.subscribe((submitedValue) =>
+      this.simpleForm.createComponent(authFormFactory);
+
+      this.component = this.simpleForm.createComponent(authFormFactory, 0);
+      this.component.instance.title = 'Changed title from dashboard';
+
+      this.component.instance.onSubmit.subscribe((submitedValue) =>
         console.log(submitedValue)
       );
+
       this.cd.detectChanges();
     }
+  }
+
+  handleDeleteComponent() {
+    this.component.destroy();
+  }
+
+  handleMoveComponent() {
+    this.simpleForm.move(this.component.hostView, 1);
   }
 }
