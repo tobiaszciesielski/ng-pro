93bef0a72242c187722ba39936748011838947e8
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-11-28 15:57:25 +0100

    Creating, modifying and delting dynamic component using ViewContainerRef

--- src/app/dashboard/components/simple-form/simple-form.component.ts
+++ src/app/dashboard/components/simple-form/simple-form.component.ts
@@ -5,17 +5,24 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./simple-form.component.scss'],
   template: `
     <div class="form">
-      <h3>Simple form</h3>
+      <h3>{{ title }}</h3>
       <form #form="ngForm" (submit)="onSubmit.emit(form.value)">
         <label class="form__label">
           <span class="form__text">Login</span>
-          <input class="form__input" type="text" name="login" id="login" />
+          <input
+            ngModel
+            class="form__input"
+            type="text"
+            name="login"
+            id="login"
+          />
         </label>
         <label class="form__label">
           <span class="form__text">Password</span>
           <input
+            ngModel
             class="form__input"
-            type="text"
+            type="password"
             name="password"
             id="password"
           />
@@ -27,4 +34,5 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class SimpleFormComponent {
   @Output() onSubmit: EventEmitter<null> = new EventEmitter();
+  title = 'Simple form';
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ComponentFactoryResolver,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { SimpleFormComponent } from './components/simple-form/simple-form.component';
 
 @Component({
   selector: 'dashboard-root',
@@ -8,8 +18,30 @@ import { Component } from '@angular/core';
       <h2 class="dashboard__title">Dashboard Component</h2>
       <dashboard-auth-panel class="dashboard__forms"></dashboard-auth-panel>
       <h2 class="dashboard__title">Simple form component</h2>
-      <dashboard-simple-form></dashboard-simple-form>
+      <div #simpleForm></div>
     </div>
   `,
 })
-export class DashboardComponent {}
+export class DashboardComponent implements AfterViewInit {
+  @ViewChild('simpleForm', { read: ViewContainerRef })
+  simpleForm!: ViewContainerRef;
+
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private cd: ChangeDetectorRef
+  ) {}
+
+  ngAfterViewInit(): void {
+    const authFormFactory =
+      this.resolver.resolveComponentFactory(SimpleFormComponent);
+
+    if (this.simpleForm) {
+      const component = this.simpleForm.createComponent(authFormFactory);
+      component.instance.title = 'Changed title from dashboard';
+      component.instance.onSubmit.subscribe((submitedValue) =>
+        console.log(submitedValue)
+      );
+      this.cd.detectChanges();
+    }
+  }
+}
